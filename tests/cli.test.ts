cbd2003ec0d6e0c7c9d14847aa0f52adb2ffdde7
Ethan Lane <ethan@vylpes.com>
2024-06-14 18:14:42 +0100

    temp: Update ls

--- tests/cli.test.ts
+++ tests/cli.test.ts
@@ -168,7 +168,7 @@ describe('query-metadata', () => {
 
 function cli(args: string[], cwd: string): Promise<cliResult> {
     return new Promise(resolve => {
-        exec("ls", { cwd }, (error, stdout, stderr) => {
+        exec("ls ./dist", { cwd }, (error, stdout, stderr) => {
             console.log(stdout);
         });
         exec(`node ${path.resolve('./dist/cli.js')} ${args.join(' ')}`,
